52934bdf03480c255a3332621cc0430ce730ccb3
MS_btff <whdghks845@gmail.com>
2025-08-02 14:51:08 +0900

    fix: 빌드 수정

--- src/main.ts
+++ src/main.ts
@@ -30,10 +30,12 @@ async function bootstrap() {
     // 로컬 개발 환경
     app.enableCors({
       origin: [
+        'https://anpay.store',
+        'https://www.anpay.store',
+        'https://dh2ep87gva43g.cloudfront.net',
+        'https://heroic-peony-7b58ca.netlify.app',
         'http://localhost:3000',
         'https://localhost:3000',
-        'http://localhost:5173', // Vite dev server
-        'http://127.0.0.1:3000',
       ],
       methods: 'GET,PUT,PATCH,POST,DELETE,OPTIONS',
       credentials: true,
